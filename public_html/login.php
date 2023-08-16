588d848511b00c1de978e379a012fb1b9cc0335c
imeriumsadiq <119714859+imeriumsadiq@users.noreply.github.com>
2023-08-16 15:58:47 +0800

    Add files via upload

---
+++ public_html/login.php
@@ -0,0 +1,45 @@
+<?php
+// Connect to the database
+$conn = mysqli_connect("localhost", "root", "", "sistempemantauanagensi");
+
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check if the form was submitted
+if ($conn) {
+    // Sanitize user input
+    $username = $_REQUEST['email'];
+    $password = $_REQUEST['pw'];
+
+    // Retrieve the user's information from the database
+    $sql = "SELECT * FROM useragensi WHERE Emel='$username' AND KataLaluan='$password'";
+    $result = mysqli_query($conn, $sql);
+
+    // Check if the query was successful
+    if (mysqli_num_rows($result) == 1) {
+        // Display a success message or redirect to a new page after successful login
+        
+        header("Location: LamanUtama.html");
+        exit();
+    } else {
+        // Display an error message
+        $errors["login"] = "Invalid username or password";
+    }
+	$sqladmin = "SELECT * FROM adminagensi WHERE Emel='$username' AND KataLaluan='$password'";
+	$result1 = mysqli_query($conn, $sqladmin);
+	
+	if (mysqli_num_rows($result1) == 1) {
+        // Display a success message or redirect to a new page after successful login
+        header("Location: LamanUtama.html");
+        exit();
+    } else {
+        // Display an error message
+        $errors["login"] = "Invalid username or password";
+    }
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
---
+++ public_html/register.php
@@ -0,0 +1,37 @@
+<?php
+
+//start session
+//session_start();
+// Connect to the database
+$conn = mysqli_connect("127.0.0.1", "root", "", "sistempemantauanagensi");
+
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$username = $_REQUEST['PIC'];
+$email = $_REQUEST['email'];
+$password = $_REQUEST['pw'];
+$check = "SELECT * FROM useragensi WHERE Emel ='$email'";
+$stmt = $conn->prepare($check);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    echo "Email Already Exist!!";
+} else {
+    $sql = "INSERT INTO useragensi (Nama, Emel, KataLaluan) VALUES ('$username', '$email', '$password')";
+    $insert_stmt = $conn->prepare($sql);
+    $insert_stmt->execute();
+    header("Location: Login_Page.html");
+}
+//// Check if the form was submitted
+//if (mysqli_query($conn, $sql)) {
+//
+//    header("Location: Login_Page.html");
+//} else {
+//    echo "ERROR: Hush! Sorry $sql. "
+//    . mysqli_error($conn);
+//}
+// Close the database connection
+mysqli_close($conn);
+?>
