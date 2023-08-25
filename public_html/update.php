1166af9c4b6738f4ee9829914699555c0bb32044
imeriumsadiq <119714859+imeriumsadiq@users.noreply.github.com>
2023-08-25 16:42:04 +0800

    Add files via upload

--- public_html/update.php
+++ public_html/update.php
@@ -1,7 +1,20 @@
 <?php
+$conn = mysqli_connect("localhost", "root", "", "sistempemantauanagensi");
+// GET CONNECTION ERRORS
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+// SQL QUERY
+$query = "SELECT * FROM infoagensi;";
+// FETCHING DATA FROM DATABASE
+$result = $conn->query($query);
 
+if ($result->num_rows > 0) {
+  
+}
+?>
 /* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+*/
 
